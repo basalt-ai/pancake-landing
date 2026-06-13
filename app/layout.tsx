185767b50371f04822c5b1c0d4973993011c3360
Guillaume <guillaume@solidy.me>
2026-06-12 21:48:35 -0700

    analytics: update GTM and conversion events

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import { Lato } from "next/font/google";
 import "./globals.css";
 import "./_styles/components.css";
+import { AnalyticsEvents } from "@/components/analytics/AnalyticsEvents";
 import { ProductHuntBadge } from "@/components/shared/ProductHuntBadge";
 
 /**
@@ -108,6 +109,9 @@ const organizationJsonLd = {
   ],
 };
 
+const googleTagManagerId = "GTM-P3Z79WKD";
+const metaPixelId = "1668160384441545";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -122,7 +126,21 @@ export default function RootLayout({
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-M37BB9RG');`,
+})(window,document,'script','dataLayer','${googleTagManagerId}');`,
+          }}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '${metaPixelId}');
+fbq('track', 'PageView');`,
           }}
         />
         {/* Organization JSON-LD — present on every page */}
@@ -138,10 +156,20 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       <body>
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-M37BB9RG"
+            src={`https://www.googletagmanager.com/ns.html?id=${googleTagManagerId}`}
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element -- Meta Pixel noscript fallback requires a raw tracking pixel. */}
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=${metaPixelId}&ev=PageView&noscript=1`}
+            alt=""
           />
         </noscript>
         <a href="#main-content" className="skip-to-content">
@@ -181,6 +209,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           }}
         />
         {children}
+        <AnalyticsEvents />
         <ProductHuntBadge />
       </body>
     </html>
